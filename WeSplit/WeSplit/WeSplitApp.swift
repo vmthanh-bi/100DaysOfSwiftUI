37be448140a8b2165bb8e3422ebc658761dae1e4
Tony Vu <vmthanh.bi@gmail.com>
2022-08-13 14:34:01 +0200

    adding WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tony Vu on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
