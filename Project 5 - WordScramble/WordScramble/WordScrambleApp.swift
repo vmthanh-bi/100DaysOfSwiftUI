1d6642f282690bc6f27b6fdc0fc0236d5a1fad97
Tony Vu <vmthanh.bi@gmail.com>
2022-08-13 18:44:11 +0200

    Words Scramble

---
+++ Project 5 - WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Tony Vu on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
